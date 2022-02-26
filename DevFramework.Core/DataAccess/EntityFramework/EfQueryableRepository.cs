db14e28ac3579ba3f6f9b099dc617168e8f4afe2
adaletinsesi1515 <memduhgumen@gmail.com>
2022-02-27 01:46:02 +0300

    Temel yapı kuruldu

---
+++ DevFramework.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T>:IQueryableRepository<T> where T:class, IEntity, new()
+    {
+        private DbContext _context;
+       // private IDbSet<T> _entities;
+
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table { get; }
+    }
+}
